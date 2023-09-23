4ab811437f5f74cd55e23c10989a0475d1b6682e
damarnez <dani.elnota@gmail.com>
2023-09-23 04:01:28 +0200

    price calculation

---
+++ contracts/src/libs/logic/PriceLogic.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+library PriceLogic {
+    uint256 constant K = 1600;
+
+    function getPrice(
+        uint256 supply,
+        uint256 amount
+    ) public pure returns (uint256) {
+        uint256 sum1 = supply == 0
+            ? 0
+            : ((supply - 1) * (supply) * (2 * (supply - 1) + 1)) / 6;
+        uint256 sum2 = supply == 0 && amount == 1
+            ? 0
+            : ((supply - 1 + amount) *
+                (supply + amount) *
+                (2 * (supply - 1 + amount) + 1)) / 6;
+        uint256 summation = sum2 - sum1;
+        return summation;
+    }
+}
