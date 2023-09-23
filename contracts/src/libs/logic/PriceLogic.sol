eb30213a4b9dcc80a0557fc7a9fa6bda5251b808
damarnez <dani.elnota@gmail.com>
2023-09-23 04:31:17 +0200

    fragment price

--- contracts/src/libs/logic/PriceLogic.sol
+++ contracts/src/libs/logic/PriceLogic.sol
@@ -4,21 +4,23 @@ pragma solidity ^0.8.19;
 library PriceLogic {
     uint256 constant K = 180;
 
-    function getPrice(
+    function getPricePerFragment() internal {}
+
+    function getPriceFramgent(
         uint256 priceFragment,
         uint256 currentSupply,
-        uint256 amountToMin
-    ) public pure returns (uint256) {
+        uint256 amountToMint
+    ) internal pure returns (uint256) {
         uint256 sum1 = currentSupply == 0
             ? 0
             : ((currentSupply - 1) *
                 (currentSupply) *
                 (2 * (currentSupply - 1) + 1)) / 6;
-        uint256 sum2 = currentSupply == 0 && amountToMin == 1
+        uint256 sum2 = currentSupply == 0 && amountToMint == 1
             ? 0
-            : ((currentSupply - 1 + amountToMin) *
-                (currentSupply + amountToMin) *
-                (2 * (currentSupply - 1 + amountToMin) + 1)) / 6;
+            : ((currentSupply - 1 + amountToMint) *
+                (currentSupply + amountToMint) *
+                (2 * (currentSupply - 1 + amountToMint) + 1)) / 6;
         uint256 summation = sum2 - sum1;
         return (priceFragment * summation) / K;
     }
