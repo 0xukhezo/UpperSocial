04b34c745bb02a6d772966800d55c67d6af76188
0xUkhezo <ukhezo.web3@gmail.com>
2023-09-22 22:52:27 -0400

    feat(pages): add all pages

---
+++ front/src/pages/notifications.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Notifications() {
+  return <div>notifications</div>;
+}
+
+export default Notifications;
---
+++ front/src/pages/proposals.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Proposals() {
+  return <div>proposals</div>;
+}
+
+export default Proposals;
